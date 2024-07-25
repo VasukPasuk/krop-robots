8ecacfd72990f846dad4c455a567c34e7cb9bf09
VasukPasuk <vasapaskivskij@gmail.com>
2024-07-25 21:51:45 +0300

    Initialisation

--- src/custom-components/ui/MainPageBlocks/@sanctions/SanctionsBlock.tsx
+++ src/custom-components/ui/MainPageBlocks/@sanctions/SanctionsBlock.tsx
@@ -16,43 +16,68 @@ function SanctionsBlock(props) {
         </>
       )}
       <div className={`sanctions-block__content-wrapper`}>
-        <h1 className={`sanctions-block__content-wrapper__title-box`}>
-          <TbCookieOff />
+        <motion.h1
+          initial={{y: -50, opacity: 0}}
+          whileInView={{y: 0, opacity: 1}}
+          transition={{duration: 1, delay: 0.25}}
+          viewport={{once: true}}
+          className={`sanctions-block__content-wrapper__title-box`}>
+          <TbCookieOff/>
           <span>Штрафні санкції</span>
-        </h1>
+        </motion.h1>
         <ul className={`sanctions-block__content-wrapper__text-blocks-wrapper`}>
-          <li className={`text-blocks-wrapper__text-block text-block-i-1`}>
+          <motion.li
+            initial={{x: -50, opacity: 0}}
+            whileInView={{x: 0, opacity: 1}}
+            transition={{duration: 1, delay: 0.25}}
+            viewport={{once: true}}
+            className={`text-blocks-wrapper__text-block text-block-i-1`}>
             <h3 className={'text-blocks-wrapper__text-block__subtitle-box'}>
               Невідповідність вимогам
             </h3>
             <span>
               Команда отримує технічну поразку, якщо не усуне невідповідність робота вимогам за 3 хвилини.
             </span>
-          </li>
-          <li className={`text-blocks-wrapper__text-block text-block-i-2`}>
+          </motion.li>
+          <motion.li
+            initial={{x: 50, opacity: 0}}
+            whileInView={{x: 0, opacity: 1}}
+            transition={{duration: 1, delay: 0.25}}
+            viewport={{once: true}}
+            className={`text-blocks-wrapper__text-block text-block-i-2`}>
             <h3 className={'text-blocks-wrapper__text-block__subtitle-box'}>
               Запізнення на матч
             </h3>
             <span>
               Команда, яка не з'явилася на матч, отримує технічну поразку.
             </span>
-          </li>
-          <li className={`text-blocks-wrapper__text-block text-block-i-3`}>
+          </motion.li>
+          <motion.li
+            initial={{x: -50, opacity: 0}}
+            whileInView={{x: 0, opacity: 1}}
+            transition={{duration: 1, delay: 0.5}}
+            viewport={{once: true}}
+            className={`text-blocks-wrapper__text-block text-block-i-3`}>
             <h3 className={'text-blocks-wrapper__text-block__subtitle-box'}>
               Втручання в матч
             </h3>
             <span>
               Учасникам заборонено торкатися роботів, поля та поверхні під час раунду.
             </span>
-          </li>
-          <li className={`text-blocks-wrapper__text-block text-block-i-4`}>
+          </motion.li>
+          <motion.li
+            initial={{x: 50, opacity: 0}}
+            whileInView={{x: 0, opacity: 1}}
+            transition={{duration: 1, delay: 0.5}}
+            viewport={{once: true}}
+            className={`text-blocks-wrapper__text-block text-block-i-4`}>
             <h3 className={'text-blocks-wrapper__text-block__subtitle-box'}>
               Затримка старту
             </h3>
             <span>
               Затримка старту раунду без дозволу судді може призвести до штрафних санкцій.
             </span>
-          </li>
+          </motion.li>
         </ul>
       </div>
     </section>
