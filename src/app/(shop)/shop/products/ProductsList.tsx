a1aa0f7e39e1d8b4a79c00409267e287da5bf2d0
VasukPasuk <vasapaskivskij@gmail.com>
2024-08-10 15:32:04 +0300

    fix: test

--- src/app/(shop)/shop/products/ProductsList.tsx
+++ src/app/(shop)/shop/products/ProductsList.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {Suspense, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import ProductCard from "@/custom-components/ui/ProductCard/ProductCard";
 import {Product} from "@prisma/client";
 import {useRouter, useSearchParams, usePathname} from "next/navigation";
@@ -35,20 +35,18 @@ function ProductsList(props: IProductsListProps) {
   const totalPagesCount = Math.ceil(products.length / MAX_ITEMS_IN_STORE_TO_VIEW)
   return (
     <>
-      <Suspense>
-        <div className="container h-full grid grid-cols-3 grid-rows-3 gap-6 pb-6 pt-6">
-          {products.slice((params.page - 1) * 9, 9 * params.page).map((product, i) => (
-            <ProductCard key={i} product={product}/>
-          ))}
-        </div>
-        <div className="container flex items-center justify-center">
-          <Pagination
-            count={totalPagesCount}
-            onChange={(event, page) => router.push(`/shop/products/?category=${params.category}&page=${page}`)}
-            size={"large"} shape={"rounded"}
-          />
-        </div>
-      </Suspense>
+      <div className="container h-full grid grid-cols-3 grid-rows-3 gap-6 pb-6 pt-6">
+        {products.slice((params.page - 1) * 9, 9 * params.page).map((product, i) => (
+          <ProductCard key={i} product={product}/>
+        ))}
+      </div>
+      <div className="container flex items-center justify-center">
+        <Pagination
+          count={totalPagesCount}
+          onChange={(event, page) => router.push(`/shop/products/?category=${params.category}&page=${page}`)}
+          size={"large"} shape={"rounded"}
+        />
+      </div>
     </>
   )
 }
