640155191e60a43b37952e88486cf878e9f1d4c6
VasukPasuk <vasapaskivskij@gmail.com>
2024-08-15 00:17:51 +0300

    design: new image, adaptive design, fix dark mode

--- src/custom-components/ui/MainPageBlocks/@carousel/Carousel.block.tsx
+++ src/custom-components/ui/MainPageBlocks/@carousel/Carousel.block.tsx
@@ -1,109 +1,89 @@
 'use client'
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './style.scss';
-import {FaArrowLeft, FaArrowRight, FaBookReader, FaBriefcase, FaRobot} from "react-icons/fa";
-import {CAROUSEL_LENGTH} from "@/constants/.names";
-import {motion} from "framer-motion";
+import { FaArrowLeft, FaArrowRight, FaBookReader, FaBriefcase, FaRobot } from "react-icons/fa";
+import { CAROUSEL_LENGTH } from "@/constants/.names";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import clsx from "clsx";
 
-
-const TAGS_DATA = [
-  {
-    text: "Проєкт Krop_Robots засновано в 2023 році, для популяризації STEM-освіти для молоді.",
-    icon: <FaBookReader className={`tag-icon`}/>
-  },
-  {
-    text: 'Сьогодні ми надаємо безкоштовні уроки, яких потребують роботодавці та сучасний бізнес - жодної сухої теорії та "Води".',
-    icon: <FaBriefcase className={`tag-icon`}/>
-  },
-  {
-    text: "Krop_Robots - це комфортне середовище, в якому ваші діти зможуть себе відчути справжніми робототехніками.",
-    icon: <FaRobot className={`tag-icon`}/>
+function MotionItem(props: { element: "h1" | "p", delay: number, children: React.ReactNode }) {
+  if (props.element === "h1") {
+    return (
+      <motion.h1
+        initial={{ x: -50, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1, delay: props.delay }}
+        viewport={{ once: true }}
+        className="text-4xl"
+      >
+        {props.children}
+      </motion.h1>
+    )
   }
-]
-
+  return (
+    <motion.p
+      initial={{ x: -50, opacity: 0 }}
+      whileInView={{ x: 0, opacity: 1 }}
+      transition={{ duration: 1, delay: props.delay }}
+      viewport={{ once: true }}
+      className="text-[1.15rem]"
+    >
+      {props.children}
+    </motion.p>
+  )
+}
 
 function CarouselBlock() {
-
   const [currentImage, setCurrentImage] = useState(0);
+
   const increaseImageNumberHandler = () => {
     setCurrentImage(prev => currentImage === (CAROUSEL_LENGTH - 1) ? 0 : (prev + 1))
   }
+
   const decreaseImageNumberHandler = () => {
     setCurrentImage(prev => currentImage === 0 ? (CAROUSEL_LENGTH - 1) : (prev - 1))
   }
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setCurrentImage(cur_img => cur_img === (CAROUSEL_LENGTH - 1) ? 0 : cur_img + 1)
-  //   }, [IMAGE_SWITCHING_DURATION])
-  //   return () => {
-  //     clearInterval(interval)
-  //   }
-  // }, [currentImage]);
+
   return (
-    <section id="public-organization-description">
-      <div className="description__title-box">
-        <motion.h1
-          initial={{x: -50, opacity: 0}}
-          whileInView={{x: 0, opacity: 1}}
-          transition={{duration: 1, delay: 0.25}}
-          viewport={{once: true}}
-          className="description__title"
-        >
+    <section className="flex flex-col gap-y-4 md:flex-row items-start justify-center container mx-auto py-16 sm:gap-x-4 ">
+      <div className="flex flex-col gap-y-4">
+        <MotionItem delay={0.25} element={"h1"}>
           Lorem ipsum dolor sit amet
-        </motion.h1>
-      </div>
-      <div className="description__content-box">
-        <motion.p
-          initial={{x: -50, opacity: 0}}
-          whileInView={{x: 0, opacity: 1}}
-          transition={{duration: 1, delay: 0.5}}
-          viewport={{once: true}}
-          className="paragraph-description"
-        >
+        </MotionItem>
+        <MotionItem element={"p"} delay={0.5}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
           est laborum.
-        </motion.p>
-        <motion.p
-          initial={{x: -50, opacity: 0}}
-          whileInView={{x: 0, opacity: 1}}
-          transition={{duration: 1, delay: 0.75}}
-          viewport={{once: true}}
-          className="paragraph-description"
-        >
+        </MotionItem>
+        <MotionItem element={"p"} delay={0.75}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
           est laborum.
-        </motion.p>
+        </MotionItem>
       </div>
-      <div id="carousel-images-container">
-        <div
-          className="images-track"
-          style={{
-            transform: `translate(-${currentImage * 100}%)`
-          }}
-        >
-          {[...new Array(CAROUSEL_LENGTH)].map((_, i) => (
-            <img loading={"lazy"} src={`/slider-image${++i}.jpg`} alt={`slider image ${++i}`} key={i}/>
-          ))}
+      <div className={"flex flex-col gap-y-4 w-full"}>
+        <div className={"w-full h-[500px] md:h-[380px] md:w-[478px] lg:w-[678px] lg:h-[560px] overflow-hidden"}>
+          <div className="w-full h-full grid grid-cols-[100%] grid-rows-[100%] grid-flow-col gap-0" style={{ transform: `translateX(-${currentImage * 100}%)`, transition: 'transform 0.7s' }}>
+            {[...Array.from({ length: 9 })].map((_, i) => (
+              <Image className={"object-cover"} width={500} height={500} key={i} alt={`Slider image ${i + 1}`} src={`/slider-image${i + 1}.jpg`} />
+            ))}
+          </div>
         </div>
-        <div className="swiper-control-footer">
-          <div
-            className="switch-image-button"
-            onClick={decreaseImageNumberHandler}
-          >
-            <FaArrowLeft/>
-          </div>
-          <div
-            className="switch-image-button"
-            onClick={increaseImageNumberHandler}
-          >
-            <FaArrowRight/>
-          </div>
+        <div className="flex flex-row gap-x-2 items-center justify-center">
+          {[...Array.from({ length: 9 })].map((_, i) => (
+            <div
+              key={i}
+              onClick={() => setCurrentImage(i)}
+              className={clsx("w-4 h-4 cursor-pointer rounded-full bg-neutral-800 transition-colors duration-500 ease-out", {
+                "bg-neutral-500": currentImage === i
+              })}
+            />
+          ))}
         </div>
       </div>
     </section>
