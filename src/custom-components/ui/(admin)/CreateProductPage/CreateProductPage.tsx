c9ad59258e2b3dad589fa9e2edee563064adf72e
VasukPasuk <vasapaskivskij@gmail.com>
2024-09-23 16:39:29 +0300

    update: product, catalog, reviews pages

--- src/custom-components/ui/(admin)/CreateProductPage/CreateProductPage.tsx
+++ src/custom-components/ui/(admin)/CreateProductPage/CreateProductPage.tsx
@@ -4,13 +4,14 @@ import CreateProductPage_ProductForm
   from "@/custom-components/ui/(admin)/CreateProductPage/CreateProductPage_ProductForm";
 import CreateProductPage_VariantsForm
   from "@/custom-components/ui/(admin)/CreateProductPage/CreateProductPage_VariantsForm";
+import React from "react";
 
 
 interface ICreateProductPageProps {
   children: React.ReactNode;
 }
 
-function CreateProductPage({children}:ICreateProductPageProps) {
+function CreateProductPage({children}: ICreateProductPageProps) {
   return (
     <div className="w-full flex flex-col gap-y-4">
       {children}
